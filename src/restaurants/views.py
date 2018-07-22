5fac567d87136ead20b9f95cee58d84f09d39313
esteb <stevjocen@gmail.com>
2018-07-22 14:59:28 +0800

    Add template inheritance.

--- src/restaurants/views.py
+++ src/restaurants/views.py
@@ -3,4 +3,11 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, "base.html", {"html_var" : True})
+    return render(request, "home.html", {"html_var" : True})
+
+def about(request):
+    return render(request, "about.html", {})
+
+def contact(request):
+    return render(request, "contact.html", {})
+    
