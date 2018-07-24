955f5d0cac7de1e6c15cefdfa43cfef1cbb8af85
esteb <stevjocen@gmail.com>
2018-07-24 21:15:16 +0800

    Implement a list view for restaurants with filter function using a slug in the url.

--- src/restaurants/views.py
+++ src/restaurants/views.py
@@ -1,7 +1,8 @@
 from django.http import HttpResponse
+from django.db.models import Q
 from django.shortcuts import render
 from django.views import View
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
 
 from .models import RestaurantLocation
 
@@ -12,3 +13,19 @@ def restaurant_listview(request):
         "object_list": queryset
     }
     return render(request, template_name, context)
+
+class RestaurantListView(ListView):
+    #template_name = 'restaurants/restaurants_list.html'
+    
+    def get_queryset(self):
+        slug = self.kwargs.get("slug")
+        
+        if slug:
+            queryset = RestaurantLocation.objects.filter(
+                Q(category__icontains = slug)|
+                Q(category__iexact = slug)
+            )
+        else:
+            queryset = RestaurantLocation.objects.all()
+        
+        return queryset
