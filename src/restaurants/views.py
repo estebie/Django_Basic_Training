3a29ff1f1cda37906ab322baae080112b0b0e800
esteb <stevjocen@gmail.com>
2018-07-25 22:24:50 +0800

    Implement a detail view using the generic template.

--- src/restaurants/views.py
+++ src/restaurants/views.py
@@ -1,8 +1,8 @@
 from django.http import HttpResponse
 from django.db.models import Q
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import View
-from django.views.generic import TemplateView, ListView
+from django.views.generic import TemplateView, ListView, DetailView 
 
 from .models import RestaurantLocation
 
@@ -15,7 +15,6 @@ def restaurant_listview(request):
     return render(request, template_name, context)
 
 class RestaurantListView(ListView):
-    #template_name = 'restaurants/restaurants_list.html'
     
     def get_queryset(self):
         slug = self.kwargs.get("slug")
@@ -29,3 +28,15 @@ class RestaurantListView(ListView):
             queryset = RestaurantLocation.objects.all()
         
         return queryset
+
+class RestaurantDetailView(DetailView):
+    queryset = RestaurantLocation.objects.all()
+
+    def get_context_data(self, **kwargs):
+        context = super(RestaurantDetailView, self).get_context_data(**kwargs)
+        return context
+
+    def get_object(self, *args, **kwargs):
+        rest_id = self.kwargs.get('rest_id')
+        obj = get_object_or_404(RestaurantLocation, id=rest_id)
+        return obj
