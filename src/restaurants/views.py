59ac0f2f50ad7666beddf29b2e263decfa153f9c
esteb <stevjocen@gmail.com>
2018-07-29 19:42:29 +0800

    Use the slug field of a restaurant location as a link for its detail page.

--- src/restaurants/views.py
+++ src/restaurants/views.py
@@ -32,11 +32,11 @@ class RestaurantListView(ListView):
 class RestaurantDetailView(DetailView):
     queryset = RestaurantLocation.objects.all()
 
-    def get_context_data(self, **kwargs):
-        context = super(RestaurantDetailView, self).get_context_data(**kwargs)
-        return context
+    # def get_context_data(self, **kwargs):
+    #     context = super(RestaurantDetailView, self).get_context_data(**kwargs)
+    #     return context
 
-    def get_object(self, *args, **kwargs):
-        rest_id = self.kwargs.get('rest_id')
-        obj = get_object_or_404(RestaurantLocation, id=rest_id)
-        return obj
+    # def get_object(self, *args, **kwargs):
+    #     rest_id = self.kwargs.get('rest_id')
+    #     obj = get_object_or_404(RestaurantLocation, id=rest_id)
+    #     return obj
