ed76cbd42797f15116117f04d38c8544747588ad
esteb <stevjocen@gmail.com>
2018-07-30 22:22:22 +0800

    Add a login page.

--- src/training/urls.py
+++ src/training/urls.py
@@ -17,6 +17,8 @@ from django.conf.urls import url
 from django.contrib import admin
 from django.views.generic import TemplateView
 
+from django.contrib.auth.views import LoginView
+
 from restaurants.views import (
     RestaurantListView, 
     RestaurantDetailView,
@@ -25,6 +27,7 @@ from restaurants.views import (
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', TemplateView.as_view(template_name='home.html')),
+    url(r'^login/$', LoginView.as_view(), name='login'),
     url(r'^restaurants/$', RestaurantListView.as_view()),
     url(r'^restaurants/create/$', RestaurantCreateView.as_view()),
     url(r'^restaurants/(?P<slug>[\w-]+)/$', RestaurantDetailView.as_view()),
