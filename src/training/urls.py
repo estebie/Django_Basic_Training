dd62b89f649d22923d6d024b3b8362d332dae736
esteb <stevjocen@gmail.com>
2018-07-31 21:43:23 +0800

    Implement django url reverse to name routes.

--- src/training/urls.py
+++ src/training/urls.py
@@ -13,7 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 from django.views.generic import TemplateView
 
@@ -26,12 +26,9 @@ from restaurants.views import (
 )
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', TemplateView.as_view(template_name='home.html')),
+    url(r'^$', TemplateView.as_view(template_name='home.html'), name='home'),
     url(r'^login/$', LoginView.as_view(), name='login'),
-    url(r'^restaurants/$', RestaurantListView.as_view()),
-    url(r'^restaurants/create/$', RestaurantCreateView.as_view()),
-    url(r'^restaurants/(?P<slug>[\w-]+)/$', RestaurantDetailView.as_view()),
-    # url(r'^restaurants/(?P<rest_id>\w+)/$', RestaurantDetailView.as_view()),
-    url(r'^contact/$', TemplateView.as_view(template_name='contact.html')),
-    url(r'^about/$', TemplateView.as_view(template_name='about.html')),
+    url(r'^resturants/', include('restaurants.urls', namespace='restaurants')),
+    url(r'^contact/$', TemplateView.as_view(template_name='contact.html'), name='contact'),
+    url(r'^about/$', TemplateView.as_view(template_name='about.html'), name='about'),
 ]
