faa69db8d33d57fb9383c168028bf4abcf48bb63
esteb <stevjocen@gmail.com>
2018-08-05 12:07:55 +0800

    Add excludes to the items model.

--- src/menus/models.py
+++ src/menus/models.py
@@ -8,11 +8,18 @@ class Item(models.Model):
     restaurant  = models.ForeignKey(RestaurantLocation)
     name        = models.CharField(max_length=120)
     contents    = models.TextField(help_text = 'Separate each item by comma')
+    excludes    = models.TextField(blank=True, null=True, help_text = 'Separate each item by comma')
     public      = models.BooleanField(default=True)
     timestamp   = models.DateTimeField(auto_now_add=True)
     updated     = models.DateTimeField(auto_now=True)
 
     class Meta:
         ordering = ['-updated', '-timestamp']
+
+    def get_contents(self):
+        return self.contents.split(",")
+
+    def get_excludes(self):
+        return self.excludes.split(",")
 
 # Create your models here.
