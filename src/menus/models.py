efce93b43d85907a4793f7f4f1455627de3afc1c
esteb <stevjocen@gmail.com>
2018-08-05 12:02:00 +0800

    Migrate menu models in database.

--- src/menus/models.py
+++ src/menus/models.py
@@ -1,3 +1,18 @@
+from django.conf import settings
 from django.db import models
+
+from restaurants.models import RestaurantLocation
+
+class Item(models.Model):
+    user        = models.ForeignKey(settings.AUTH_USER_MODEL)
+    restaurant  = models.ForeignKey(RestaurantLocation)
+    name        = models.CharField(max_length=120)
+    contents    = models.TextField(help_text = 'Separate each item by comma')
+    public      = models.BooleanField(default=True)
+    timestamp   = models.DateTimeField(auto_now_add=True)
+    updated     = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-updated', '-timestamp']
 
 # Create your models here.
--- src/training/settings/base.py
+++ src/training/settings/base.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'widget_tweaks',
     'django.contrib.staticfiles',
-
+    'menus',
     'restaurants'
 ]
 
