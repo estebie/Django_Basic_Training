a693ae84a10e100e94446eff070728f8bb383894
esteb <stevjocen@gmail.com>
2018-08-05 14:31:32 +0800

    Add a create menu item page.

--- src/menus/views.py
+++ src/menus/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import ListView, DetailView, CreateView, UpdateView
 
 from .forms import ItemForm
@@ -12,13 +13,33 @@ class ItemDetailView(DetailView):
     def get_queryset(self):
         return Item.objects.filter(user=self.request.user)
 
-class ItemCreateView(CreateView):
+class ItemCreateView(CreateView, LoginRequiredMixin):
     form_class = ItemForm
-    def get_queryset(self):
-        return Item.objects.filter(user=self.request.user)
+    login_url = '/login/'
+    template_name = 'form.html'
+    
+    def form_valid(self, form):
+        instance = form.save(commit=False)
+        instance.user = self.request.user
+        return super(ItemCreateView, self).form_valid(form)
+    
+    def get_context_data(self, **kwargs):
+        context = super(ItemCreateView, self).get_context_data(**kwargs)
+        context['title'] = "Add Item"
+        return context     
 
-class ItemUpdateView(ListView):
+class ItemUpdateView(UpdateView, LoginRequiredMixin):
     form_class = ItemForm
-    def get_queryset(self):
-        return Item.objects.filter(user=self.request.user)       
+    login_url = '/login/'
+    template_name = 'form.html'
+    
+    def form_valid(self, form):
+        instance = form.save(commit=False)
+        instance.user = self.request.user
+        return super(ItemUpdateView, self).form_valid(form)
+    
+    def get_context_data(self, **kwargs):
+        context = super(ItemUpdateView, self).get_context_data(**kwargs)
+        context['title'] = "Update Item"
+        return context           
 # Create your views here.
--- src/restaurants/views.py
+++ src/restaurants/views.py
@@ -37,10 +37,15 @@ class RestaurantDetailView(DetailView):
 class RestaurantCreateView(LoginRequiredMixin, CreateView):
     form_class = RestaurantLocationCreateForm
     login_url = '/login/'
-    template_name = 'restaurants/form.html'
+    template_name = 'form.html'
     success_url = '/restaurants'
 
     def form_valid(self, form):
         instance = form.save(commit=False)
         instance.owner = self.request.user
         return super(RestaurantCreateView, self).form_valid(form)
+    
+    def get_context_data(self, **kwargs):
+        context = super(RestaurantCreateView, self).get_context_data(**kwargs)
+        context['title'] = "Add Restaurant"
+        return context
